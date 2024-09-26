e12e98f53f28ce1eefbaf93ee95542946ea9cbaa
FromSi <fromsi665@gmail.com>
2024-09-26 17:40:56 +0500

    update: optimisation regexp for ram

--- regex_with_emoji.go
+++ regex_with_emoji.go
@@ -5,14 +5,14 @@ import (
 	"compress/flate"
 	"encoding/base64"
 	"encoding/json"
-	"io"
 	"math/rand"
 	"regexp"
 	"strings"
 )
 
 type RegexWithEmoji struct {
-	data map[string][]string
+	data  map[string][]string
+	regex map[string]*regexp.Regexp
 }
 
 func NewRegexWithEmoji(data string) (*RegexWithEmoji, error) {
@@ -26,41 +26,36 @@ func NewRegexWithEmoji(data string) (*RegexWithEmoji, error) {
 
 	defer reader.Close()
 
-	var buf bytes.Buffer
-
-	_, err = io.Copy(&buf, reader)
-
-	if err != nil {
-		return nil, err
-	}
-
 	var dataJson map[string][]string
 
-	err = json.Unmarshal([]byte(buf.String()), &dataJson)
-
-	if err != nil {
+	if err := json.NewDecoder(reader).Decode(&dataJson); err != nil {
 		return nil, err
 	}
 
-	return &RegexWithEmoji{data: dataJson}, nil
+	regexMap := make(map[string]*regexp.Regexp)
+
+	for k := range dataJson {
+		regexMap[k] = regexp.MustCompile(k)
+	}
+
+	return &RegexWithEmoji{data: dataJson, regex: regexMap}, nil
 }
 
-func (receiver RegexWithEmoji) GetEmoji(text string) string {
+func (receiver *RegexWithEmoji) GetEmoji(text string) string {
 	for k, v := range receiver.data {
 		if receiver.matches(text, k) {
 			return receiver.getRandomStringByMap(v)
 		}
 	}
-
 	return ""
 }
 
-func (receiver RegexWithEmoji) matches(text string, template string) bool {
-	regex := regexp.MustCompile(template)
+func (receiver *RegexWithEmoji) matches(text string, template string) bool {
+	regex := receiver.regex[template]
 
 	return regex.FindAllString(text, -1) != nil
 }
 
-func (receiver RegexWithEmoji) getRandomStringByMap(data []string) string {
+func (receiver *RegexWithEmoji) getRandomStringByMap(data []string) string {
 	return data[rand.Intn(len(data))]
 }
